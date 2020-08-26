b275a7d8b08f310e5d457a291febdcbae1aacc28
Koichiro Eto <koichiro.eto@gmail.com>
2020-08-27 01:54:29 +0900

    Update.

--- MacInjectionTest/ViewController.swift
+++ MacInjectionTest/ViewController.swift
@@ -9,19 +9,24 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
     @objc func injected() {
+        //print("start injected")
         show()
+        //print("end injected")
     }
     override func viewDidLoad() {
+        //print("start viewDidLoad")
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.injected), name: NSNotification.Name(rawValue: "INJECTION_BUNDLE_NOTIFICATION"), object: nil)
         show()
     }
     func show() {
-        let label = NSTextField(frame: NSRect(x: 20, y: 20, width: 150, height: 50))
+        //print("start show")
+        let label = NSTextField(frame: NSRect(x: Int.random(in: 10..<100), y: Int.random(in: 10..<100), width: 150, height: 50))
         label.backgroundColor = NSColor.cyan
         label.stringValue = "Hello, world!"
         view.addSubview(label)
+        //print("end show")
     }
 
     override var representedObject: Any? {
